fc2201a4bad80f1c1fbb490c20f3fcdece71aca5
Cristian Gomez <2912196670108@ingenieria.usac.edu.gt>
2020-08-01 20:13:14 -0600

    Nodo,obj Student, menu

---
+++ Tareas/Tarea1/ListaSimple/ListaSimple/SimpleList.cpp
@@ -0,0 +1,93 @@
+#include "SimpleList.h"
+#include "Nodo.h"
+#include "Student.h"
+#include <iostream>
+using namespace std;
+
+SimpleList::SimpleList()
+{
+	head = nullptr;
+	tail = nullptr;
+}
+
+void SimpleList::add(Student student) 
+{
+	Nodo* newNodo;
+	newNodo = new Nodo(student);
+
+	if (head == nullptr)
+	{
+		newNodo->setNext(nullptr);
+		head = newNodo;
+		tail = newNodo;
+	}
+	else
+	{
+		tail->setNext(newNodo);
+		tail = newNodo;
+	}
+}
+
+Nodo* SimpleList::search(int idStudent)
+{
+	Nodo* aux = this->head;
+
+	while (aux != nullptr)
+	{
+		if (aux->getNodo().getIdStudent() == idStudent)
+		{
+			return aux;
+		}
+		aux = aux->getNext();
+	}
+
+	return nullptr;
+}
+
+void SimpleList::delete_student(int idStudent)
+{
+	Nodo* aux = this->head;
+	Nodo* previo = nullptr;
+
+	if (aux != nullptr)
+	{
+		while (aux != nullptr)
+		{
+			if (aux->getNodo().getIdStudent() == idStudent)
+			{	
+				previo->setNext(aux->getNext());
+
+				if (aux == this->head)
+				{
+					this->head = aux->getNext();
+					break;
+				}
+				else if (aux == this->tail)
+				{
+					this->tail = previo;
+					break;
+				}
+			}
+			previo = aux;
+			aux = aux->getNext();
+		}
+	}
+	else
+	{
+		cout << "No hay elementos!!" << endl;
+	}
+	delete aux;
+}
+
+void SimpleList::print()
+{
+	Nodo* aux = this->head;
+	while (aux != nullptr)
+	{
+		cout << "Carnet: " << aux->getNodo().getIdStudent()
+			<< " Nombre: " << aux->getNodo().getName() << endl;
+		aux = aux->getNext();
+	}
+}
+
+SimpleList::~SimpleList() {}
---
+++ Tareas/Tarea1/ListaSimple/ListaSimple/SimpleList.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Nodo.h"
+#include "Student.h"
+
+class SimpleList
+{
+public:
+	Nodo* head;
+	Nodo* tail;
+
+	SimpleList();
+	~SimpleList();
+	
+	void add(Student student);
+	Nodo* search(int idStudent);
+	//Nodo* search(string idStudent);
+	void delete_student(int idStudent);
+	void print();
+};
+
